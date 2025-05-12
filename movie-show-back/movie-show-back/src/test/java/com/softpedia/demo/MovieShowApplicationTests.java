d83613f8a00b77acd50b06e1b0af1f48a9193260
salamaaa <mohamed.salamafciscu@gmail.com>
2025-05-12 21:44:51 +0200

    adding the spring boot basic structure

---
+++ movie-show-back/movie-show-back/src/test/java/com/softpedia/demo/MovieShowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softpedia.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieShowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
