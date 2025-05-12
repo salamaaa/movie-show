bb72f915a89a936c48e700caa7660cf7ff8b6094
salamaaa <mohamed.salamafciscu@gmail.com>
2025-05-12 22:32:08 +0200

    refactoring pom.xml file

--- movie-show-back/movie-show-back/src/main/java/com/softpedia/demo/entity/Movie.java
+++ movie-show-back/movie-show-back/src/main/java/com/softpedia/demo/entity/Movie.java
@@ -1,4 +1,79 @@
 package com.softpedia.demo.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Movie {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String imdbId;
+    private String title;
+    private String year;
+    private String posterUrl;
+    private String plot;
+
+    public Movie() {
+    }
+
+    public Movie(Long id, String imdbId, String title, String year, String posterUrl, String plot) {
+        this.id = id;
+        this.imdbId = imdbId;
+        this.title = title;
+        this.year = year;
+        this.posterUrl = posterUrl;
+        this.plot = plot;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getImdbId() {
+        return imdbId;
+    }
+
+    public void setImdbId(String imdbId) {
+        this.imdbId = imdbId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+    public String getPosterUrl() {
+        return posterUrl;
+    }
+
+    public void setPosterUrl(String posterUrl) {
+        this.posterUrl = posterUrl;
+    }
+
+    public String getPlot() {
+        return plot;
+    }
+
+    public void setPlot(String plot) {
+        this.plot = plot;
+    }
 }
